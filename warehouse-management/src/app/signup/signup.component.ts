dcdf56394a4d9da2cf8c884fc8097f9d39412a89
Hariharan A <hari3027@global.chainsys.com>
2022-06-02 22:11:44 +0530

    project

--- warehouse-management/src/app/signup/signup.component.ts
+++ warehouse-management/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder,Validators, NgForm } from '@angular/forms';
+import { FormGroup,FormBuilder,Validators} from '@angular/forms';
 import { ApiCallService } from '../api-call.service';
 
 @Component({
